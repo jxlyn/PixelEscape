7aac14b58b58f7b56d622d2a3e47cbbcbdfaab87
Felix <huj15@rpi.edu>
2022-08-18 01:28:12 -0400

    DONT TOUCH LAUCHER

--- src/pixelEscape/Laucher.java
+++ src/pixelEscape/Laucher.java
@@ -1,17 +1,20 @@
-
 package pixelEscape;
 
 import javax.swing.*;
 
+/**
+ * This is the driver class contain main method
+ *
+ */
 public class Laucher {
-  
-  public static void main(String[] args) {     
-    try {
-      UIManager.setLookAndFeel( UIManager.getCrossPlatformLookAndFeelClassName() );
-    } catch (Exception e) {
-      e.printStackTrace();
+    public static void main(String[] args) {
+        try {
+          UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+        
+        Frame_Main frame = new Frame_Main();
+        Controller_Main controller = new Controller_Main(frame);
     }
-    new Pixel_Escape();
-  }
-  
 }
